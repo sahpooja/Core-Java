856e77bdc4ecd47d0e997aceb35263db4209dd8e
lenovo <lenovo@lenovo-PC-4>
2024-01-18 13:35:41 +0530

    commit

---
+++ DayOne/src/com/edu/comple/CompleDemo.java
@@ -0,0 +1,26 @@
+package com.edu.comple;
+
+public class CompleDemo {
+
+	public static void main(String[] args) 
+	{
+		byte num1=127;
+		short num2= 3456;
+		int num3= 123456;
+		long num4= 937222424;
+		char var= 'c';
+		String st="Edubride";
+		float num5=53.6f;
+		double num6=54.78d;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+		System.out.println(var);
+		System.out.println(st);
+
+	}
+
+}
